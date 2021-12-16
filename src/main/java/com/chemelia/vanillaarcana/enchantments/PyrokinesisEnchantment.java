4458b316ec88efe6af2f35b182cc1bf0055e0d72
videogame hacker <half-kh-hacker@hackery.site>
2021-12-16 19:50:35 +0000

    pewpewpew!!! pyrokinesis right click spawns fireball

--- src/main/java/com/chemelia/vanillaarcana/enchantments/PyrokinesisEnchantment.java
+++ src/main/java/com/chemelia/vanillaarcana/enchantments/PyrokinesisEnchantment.java
@@ -11,9 +11,11 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeConfig.Server;
 
 ///////////////
@@ -62,45 +64,12 @@ public void onSwingItem(){
 public class PyrokinesisEnchantment extends Enchantment {
     public static final String ID = VanillaArcana.MOD_ID + ":pyrokinesis";
 
-    @Override
-    public int getMaxLevel(){
-        return 5;
-    }
-
-    @Override
-    public void doPostAttack(LivingEntity attacker, Entity target, int enchantmentLevel){
-        //if on a server:
-        if (!attacker.level.isClientSide()){
-            ServerLevel world = (ServerLevel) attacker.level;
-            ServerPlayer player = ((ServerPlayer) attacker);
-            BlockPos position = target.blockPosition();
-
-
-
-            switch(enchantmentLevel){
-                case 1:
-                    EntityType.SMALL_FIREBALL.spawn(world, null, player, position, 
-                        MobSpawnType.TRIGGERED, true, true);
-                    break;
-                case 2:
-                    EntityType.FIREBALL.spawn(world, null, player, position,
-                        MobSpawnType.TRIGGERED, true, true);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    
     public PyrokinesisEnchantment() {
         super(Rarity.UNCOMMON, RegistryHandler.WAND_CATEGORY, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
-        
     }
 
-    
-    // private static boolean canEnchantItem(Item item){
-        
-    // }
-    
+    @Override
+    public int getMaxLevel(){
+        return 3;
+    }
 }
--- src/main/java/com/chemelia/vanillaarcana/item/custom/WandItem.java
+++ src/main/java/com/chemelia/vanillaarcana/item/custom/WandItem.java
@@ -1,14 +1,61 @@
 package com.chemelia.vanillaarcana.item.custom;
 
+import com.chemelia.vanillaarcana.RegistryHandler;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class WandItem extends Item {
-
     public WandItem(Properties pProperties) {
         super(pProperties);
-        
     }
 
-    
+    @Override
+    public boolean isEnchantable(ItemStack pStack) {
+        return true;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand pUsedHand) {
+        ItemStack stack = player.getItemInHand(pUsedHand);
+        if (!(stack.getItem() instanceof WandItem))
+            return InteractionResultHolder.pass(player.getItemInHand(pUsedHand));
+
+        // TODO: Probably put this in, like, its own handler thing for Pyrokinesis :)
+        int pyrokinesisLevel = EnchantmentHelper.getItemEnchantmentLevel(RegistryHandler.PYROKINESIS.get(), stack);
+        if (pyrokinesisLevel != 0) {
+            if (!world.isClientSide()) {
+                Vec3 look = player.getLookAngle();
+                Vec3 pos = player.getEyePosition().add(look.scale(0.9));
+                Vec3 velocity = look.scale(0.5);
+
+                SmallFireball fireball = new SmallFireball(world, player, 0, 0, 0);
+                fireball.setPos(pos.x, pos.y, pos.z);
+                fireball.xPower = velocity.x;
+                fireball.yPower = velocity.y;
+                fireball.zPower = velocity.z;
+
+                world.addFreshEntity(fireball);
+
+                player.getCooldowns().addCooldown(this, 20 / pyrokinesisLevel);
+
+                return InteractionResultHolder.success(stack);
+            }
+        }
+
+        return InteractionResultHolder.pass(player.getItemInHand(pUsedHand));
+    }
+
+    @Override
+    public int getItemEnchantability(ItemStack stack) {
+        return 20;
+    }
 }
